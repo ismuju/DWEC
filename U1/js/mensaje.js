4672faea2a297a0093fe092dc9bf119091b6d4a5
Ismael <ismael146@hotmail.com>
2017-09-19 20:45:24 +0200

    19/09/17

---
+++ U1/js/mensaje.js
@@ -0,0 +1,6 @@
+//alert(t); //MUESTRA VALOR DE LA VARIABLE t
+console.log("Esto es una cadena"); //MUESTRA EL MENSAJE EN LA CONSOLA DEL NAVEGADOR
+console.info("Esto es info"); //TIPO DE AVISOS
+console.warn("Esto es un aviso"); //TIPO DE AVISOS
+console.error("Esto es un error"); //TIPO DE AVISOS
+document.write("<h1>HEY BROTHER!</h1>"); //LO INCORPORA EN EL HTML (o sobrescribe), EL LUGAR DEPENDE DE DONDE LEA EL JS, MEJOR NO USAR
