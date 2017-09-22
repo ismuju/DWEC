48e87c5546b755d3c34b758b283d5226e808df7f
Ismael <ismael146@hotmail.com>
2017-09-22 15:43:59 +0200

    Ejemplo

--- Ejemplos/scriptDeclaracionesTipos.js
+++ Ejemplos/scriptDeclaracionesTipos.js
@@ -1,20 +1,20 @@
-
-//****** Explicación sobre el uso estricto:
-//Sin activar use strict no hay problemas si no declaramos variables:
+//
+// //****** Explicación sobre el uso estricto:
+// //Sin activar use strict no hay problemas si no declaramos variables:
 // var x = 2;
 // var y = 3;
 // z = x + y;
 // console.log(z);
-
+//
 // //Cuando activamos use strict debemos declarar variables o da error:
-// "use strict"
-// var x = 2;
-// var y = 3;
-// z = x + y;
-// console.log(z);
-
-
-
+"use strict";
+var x = 2;
+var y = 3;
+z = x + y;
+console.log(z);
+//
+//
+//
 // //***** Diferencia entre null, undefined y no declarar la variable
 // //null: x se ha declarado pero no tiene valor. Sí tiene tipo porque se
 // //le ha dado el valor de null
@@ -34,9 +34,9 @@
 // //Esto no es una cadena vacía porque tiene el valor espacio en blanco.
 // var s = " ";
 // console.log(s);
-
-
-//**** Diferencia entre Infinite y NaN
+//
+//
+// //**** Diferencia entre Infinite y NaN
 // //Infinito es un número
 // var z = 3/0;
 // console.log(z);
@@ -49,19 +49,19 @@
 // console.log("Infinito - Infinito es " + (Infinity - Infinity));
 // console.log("Infinito + Infinito es " + (Infinity + Infinity));
 // console.log("Infinito*5 es " + (Infinity*5));
-
+//
 // //Concatenación estricta
 // var string = "Hola";
 // // string += " Mundo";
 // string = string + " Mundo";
 // console.log(string + "!");
-
-
+//
+//
 // // ***** Operadores normales: +, -, *, /
 // console.log((5 + 4) / 3);
 // console.log(undefined / 5);
-
-
+//
+//
 // // ***** Igualdad
 // var x = 4, y = 4;
 // if (x == y) {
@@ -72,8 +72,8 @@
 // if (x == y) {
 //   console.log("x='4' es igual a y=4");
 // }
-
-
+//
+//
 // // ***** Igualdad estricta
 // if (x === y) {
 //   console.log("Estricta: x='4' es igual a y=4");
@@ -82,7 +82,7 @@
 //   console
 //   .log("Estricta: x='4' es distinto de y=4");
 // }
-
+//
 // // ***** If todo false
 // if ( false || null || undefined || "" || 0 || NaN) {
 //   console.log("Este línea nunca se ejecutará");
@@ -90,7 +90,7 @@
 // else {
 //   console.log ("Son todos unos falsos");
 // }
-
+//
 // // ***** If todo true
 // if (true && "hello" && 1 && -1 && "false") {
 //   console.log("Todo verdad");
