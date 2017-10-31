475f83091ee7700401008b42621ac8c6aa1c874f
Ismael <ismael146@hotmail.com>
2017-10-31 20:28:49 +0100

    COOKIES

--- U2/Practica3Tienda/scriptCarrito.js
+++ U2/Practica3Tienda/scriptCarrito.js
@@ -1,15 +1,21 @@
+// a. Comprueba cuando se carga que el usuario tiene las cookies habilitadas y muestra un aviso si no es así.
+if (!navigator.cookieEnabled) {
+  alert("ACTIVA LAS COOKIES");
+}
 
-El fichero scriptCarrito:
-    a. Comprueba cuando se carga que el usuario tiene las cookies
-    habilitadas y muestra un aviso si no es así.
-    b. Inicializa el objeto Usuario con los datos del usuario actual, calcula el
-    importe de la compra y la muestra.
-    c. Definirá las funciones que se emplean en carrito:
-          i. eliminar: elimina el último producto de la lista y actualiza el importe y el dibujo
-          ii. hacerPedido: si hay productos, muestra un mensaje de
-          confirmación para aceptar las condiciones del pedido y una
-          vez confirmado carga la página pedidoOK.html
-          iii. condCompraOnline: muestra en otra ventana emergente las
-          condiciones de compra y un botón que permite cerrarlas.
-          iv. Inicio: vuelve a home.html
-          v. Imprimir: imprime la página del carrito
+
+// b. Inicializa el objeto Usuario con los datos del usuario actual, calcula el importe de la compra y la muestra.
+
+
+
+
+
+// c. Definirá las funciones que se emplean en carrito:
+//       i. eliminar: elimina el último producto de la lista y actualiza el importe y el dibujo
+//       ii. hacerPedido: si hay productos, muestra un mensaje de
+//       confirmación para aceptar las condiciones del pedido y una
+//       vez confirmado carga la página pedidoOK.html
+//       iii. condCompraOnline: muestra en otra ventana emergente las
+//       condiciones de compra y un botón que permite cerrarlas.
+//       iv. Inicio: vuelve a home.html
+//       v. Imprimir: imprime la página del carrito
